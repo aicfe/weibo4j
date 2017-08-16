1c7559edc613b6dd9b74c3fba61a77b37dfd7f7f
longshine <longxianghe@gmail.com>
2017-08-16 15:25:23 +0800

    clean up while loading config

--- src/main/java/weibo4j/util/WeiboConfig.java
+++ src/main/java/weibo4j/util/WeiboConfig.java
@@ -2,25 +2,43 @@ package weibo4j.util;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class WeiboConfig {
-	public WeiboConfig(){}
-	private static Properties props = new Properties(); 
-	static{
-		try {
-			props.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("sinaconfig.properties"));
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+
+	public WeiboConfig() {
 	}
-	public static String getValue(String key){
+
+	private static Properties props = new Properties();
+
+	static {
+		InputStream in = Thread.currentThread().getContextClassLoader().getResourceAsStream("sinaconfig.properties");
+		if (in != null) {
+			try {
+				props.load(in);
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			} finally {
+				try {
+					in.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+
+	}
+
+	public static String getValue(String key) {
 		return props.getProperty(key);
 	}
 
-    public static void updateProperties(String key,String value) {    
-            props.setProperty(key, value); 
-    } 
+	public static void updateProperties(String key, String value) {
+		props.setProperty(key, value);
+	}
+
 }
